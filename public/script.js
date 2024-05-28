c2ed6a07076e2522be46d4fccee84a4240eb9a8b
zainablfz <144009548+zainablfz@users.noreply.github.com>
2024-05-28 21:39:15 +0200

    client-side fetch #14

--- public/script.js
+++ public/script.js
@@ -31,73 +31,41 @@ btn.addEventListener('click', () => {
     });
 
 
-    // Selecteer like formulier
-let form = document.querySelector('form')
+
+// Selecteer share formulier
+let form = document.querySelector('form');
 
 // Luister naar het submit event
 form.addEventListener('submit', function(event) {
+    // Voorkom dat het formulier de pagina herlaadt
+    event.preventDefault();
 
-	let shareBtn = document.querySelector('.share-button')
-	shareBtn.classList.add('.active-share')
+    // Selecteer de .shares div
+    let shares = document.querySelector('.shares');
 
-	// Het this object refereert hier naar het formulier zelf
+    // Lees de data van het formulier in
+    let data = new FormData(this);
 
-	// Lees de data van het formulier in
-	// https://developer.mozilla.org/en-US/docs/Web/API/FormData
-	let data = new FormData(this)
+    // Voeg een extra eigenschap aan de formulierdata toe
+    data.append('enhanced', true);
 
-	// Voeg een extra eigenschap aan de formulierdata toe
-	// Deze gaan we server-side gebruiken om iets anders terug te sturen
-	data.append('enhanced', true)
+    // Gebruik een client-side fetch om een POST te doen naar de server
+    fetch(this.action, {
+        method: this.method,
+        body: data // Gebruik FormData object als body
+    }).then(function(response) {
+        // Als de server een antwoord geeft, krijgen we een stream terug
+        // We willen hiervan de text gebruiken, wat in dit geval HTML teruggeeft
+        return response.text();
+    }).then(function(responseHTML) {
+        // Creëer een tijdelijke div om de HTML te parseren
+        let tempDiv = document.createElement('div');
+        tempDiv.innerHTML = responseHTML;
 
-	// Waarschijnlijk wil je op deze plek ook een loading state
-	// maken, maar daar gaan we volgende week mee aan de slag
-	form.classList.add('is-loading')
+        // Selecteer alleen de inhoud van .shares uit de ontvangen HTML
+        let newSharesContent = tempDiv.querySelector('.shares').innerHTML;
 
-	// Gebruik een client-side fetch om een POST te doen naar de server
-	// Als URL gebruiken we this.action
-	// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-	fetch(this.action, {
-
-		// Als method gebruiken we this.method (waarschijnlijk POST)
-		method: this.method,
-
-		// Als body geven de data van het formulier mee (inclusief de extra eigenschap)
-		// https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
-		body: new URLSearchParams(data)
-
-	}).then(function(response) {
-		// Als de server een antwoord geeft, krijgen we een stream terug
-		// We willen hiervan de text gebruiken, wat in dit geval HTML teruggeeft
-		return response.text()
-
-	}).then(function(responseHTML) {
-		// En de HTML kunnen we gebruiken om onze DOM aan te passen
-
-		//document.querySelector('body').innerHTML = responseHTML
-		//console.log(responseHTML)
-  
-		// Het is gelukt, neem de waarde uit de span en tel er een bij op
-		
-		if (document.startViewTransition) {
-			document.startViewTransition(function() {
-				document.querySelector('.likes').outerHTML = responseHTML
-			});
-		} else {
-			document.querySelector('.likes').outerHTML = responseHTML
-		}
-
-		// En hier kun je bijvoorbeeld nog wat extra's doen om duidelijker te maken
-		// dat er iets gebeurd is op de pagina
-		
-		// Een eventuele loading state haal je hier ook weer weg
-		form.classList.remove('is-loading')
-	});
-
-	// Als alles gelukt is, voorkom dan de submit van de browser zelf
-	// Stel dat je hierboven een tikfout hebt gemaakt, of de browser ondersteunt
-	// een bepaalde feature hierboven niet (bijvoorbeeld FormData), dan krijg je
-	// een error en wordt de volgende regel nooit uitgevoerd. De browser valt dan
-	// automatisch terug naar de standaard POST, wat prima is.
-	event.preventDefault()
-})
+        // Hier wordt alleen de .shares div geüpdate met de nieuwe inhoud
+        shares.innerHTML = newSharesContent;
+    });
+});
