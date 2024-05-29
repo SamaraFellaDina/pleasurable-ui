bdcd7694833ab0f7a0156e6a24a34a80930bb2c8
SamaraFellaDina <sammybensink@gmail.com>
2024-05-29 10:25:48 +0200

    js bug resolved

--- public/script.js
+++ public/script.js
@@ -1,3 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const carousel = document.getElementById('featured-carousel');
+  let items = Array.from(carousel.children);
+  
+  // Duplicate the items to create the infinite effect
+  items.forEach(item => {
+    const clone = item.cloneNode(true);
+    carousel.appendChild(clone);
+  });
+
+  // Adjust the scroll position to the middle of the duplicated items
+  carousel.scrollLeft = items[0].clientWidth * items.length;
+  const itemWidth = items[0].clientWidth;
+
+  // Handle the scroll event
+  carousel.addEventListener('scroll', () => {
+    const scrollLeft = carousel.scrollLeft;
+    const itemWidth = items[0].clientWidth;
+    const totalWidth = itemWidth * items.length;
+
+    if (scrollLeft >= totalWidth) {
+      // Move to the original set of items
+      carousel.scrollLeft = scrollLeft - totalWidth;
+    } else if (scrollLeft <= 0) {
+      // Move to the duplicated set of items
+      carousel.scrollLeft = scrollLeft + totalWidth;
+    }
+  });
+});
+
+
 const btn = document.querySelector('.share-button');
 
 function fire (ratio, opt) {
@@ -70,32 +101,3 @@ form.addEventListener('submit', function(event) {
     });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.getElementById('featured-carousel');
-    let items = Array.from(carousel.children);
-    
-    // Duplicate the items to create the infinite effect
-    items.forEach(item => {
-      const clone = item.cloneNode(true);
-      carousel.appendChild(clone);
-    });
-  
-    // Adjust the scroll position to the middle of the duplicated items
-    carousel.scrollLeft = items[0].clientWidth * items.length;
-    const itemWidth = items[0].clientWidth;
-  
-    // Handle the scroll event
-    carousel.addEventListener('scroll', () => {
-      const scrollLeft = carousel.scrollLeft;
-      const itemWidth = items[0].clientWidth;
-      const totalWidth = itemWidth * items.length;
-  
-      if (scrollLeft >= totalWidth) {
-        // Move to the original set of items
-        carousel.scrollLeft = scrollLeft - totalWidth;
-      } else if (scrollLeft <= 0) {
-        // Move to the duplicated set of items
-        carousel.scrollLeft = scrollLeft + totalWidth;
-      }
-    });
-});
